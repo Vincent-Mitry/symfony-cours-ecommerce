0c180da00efcd61c5369d1d4d5e4a69726b3ad6c
Vincent Mitry <vinz.mitry@gmail.com>
2021-09-14 12:00:51 +0200

    http foundation - requete et reponse http

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class TestController {
 
     public function index() {
         dd("Ca fonctionne");
     }
 
-    public function test() {
-        dd("Page de test");
+    public function test(Request $request) // La ligne entre parenthèses équivaut à la ligne en dessous
+    {
+        // $request = Request::createFromGlobals(); // Request: Objet dont on se sert pour analyser les requêtes HTTP
+
+        // dump($request);
+
+        $age = $request->query->get('age', 0); // Méthode qui nous permet de récupérer des infos avec des paramètres par défaut
+
+        return new Response("Vous avez $age ans !"); // Response : Classe de HTTP Foudation
+
     }
 
 
