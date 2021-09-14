1b0e3eb4169bfc112f07a6664352dd746fc9119c
Vincent Mitry <vinz.mitry@gmail.com>
2021-09-14 12:26:23 +0200

    paramètres de routes, valeurs par défaut, requirements

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,16 +11,12 @@ class TestController {
         dd("Ca fonctionne");
     }
 
-    public function test(Request $request) // La ligne entre parenthèses équivaut à la ligne en dessous
+    public function test(Request $request, $age) // On peut ajouter $age en paramètre et supprimer la ligne du dessous (Symfony reconnaît la paramètre 'age' passé dnas les fichier de routes)
     {
-        // $request = Request::createFromGlobals(); // Request: Objet dont on se sert pour analyser les requêtes HTTP
 
-        // dump($request);
-
-        $age = $request->query->get('age', 0); // Méthode qui nous permet de récupérer des infos avec des paramètres par défaut
+        // $age = $request->attributes->get('age', 0); // On retrouve les paramètres de la route (configuré dans routes.yaml -> /{age}) dans les attributs de la request
 
         return new Response("Vous avez $age ans !"); // Response : Classe de HTTP Foudation
-
     }
 
 
