2bf9bf7eb719e46a8589c2313658505a8fe34330
Vincent Mitry <vinz.mitry@gmail.com>
2021-09-14 11:29:55 +0200

    Creation des routes

---
+++ src/Controller/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+class TestController {
+
+    public function index() {
+        dd("Ca fonctionne");
+    }
+
+    public function test() {
+        dd("Page de test");
+    }
+
+
+}
+
