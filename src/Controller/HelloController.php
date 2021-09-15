3fed131ea85d8c2f839d5a3e50fda06c01c8da94
Vincent Mitry <vinz.mitry@gmail.com>
2021-09-15 11:35:07 +0200

    notion de services

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController {
 
+    // On injecte notre dépendance dans le constructeur (méthode qui fonctionne pour toutes le classes)
+    
+    // protected $logger;
+
+    // public function __construct(LoggerInterface $logger)
+    // {
+    //     $this->logger = $logger;
+    // }
+
+    // Pour le controllers (uniquement), on peut l'injecter dans les paramètres d'une méthode
      /**
      * @Route("/hello/{name}", name="hello", methods={"GET", "POST"}, host="localhost", schemes={"http", "https"})
      */
-    public function hello(string $name = "World")
+    public function hello(string $name = "World", LoggerInterface $logger)
     {
-
+        // A chaque fois qu'on va appeler cette route, ce message de log va s'afficher dans la console
+        // $this->logger->info("Mon message de log !");
+        $logger->info("Mon message de log !");
         return new Response("Hello $name");
     } 
 }
