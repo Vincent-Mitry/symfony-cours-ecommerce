953ae5c6c4ab78b1e75592db8f7321cce637c392
Vincent Mitry <vinz.mitry@gmail.com>
2021-09-23 17:12:23 +0200

    configuration d'un seuil de tva

--- src/Taxes/Detector.php
+++ src/Taxes/Detector.php
@@ -4,15 +4,20 @@ namespace App\Taxes;
 
 class Detector {
 
+    protected $seuil;
+
+    public function __construct(float $seuil)
+    {
+        $this->seuil = $seuil;
+    }
+
     public function detect(float $amount) : bool {
 
-        if ($amount > 100){
+        if ($amount > $this->seuil){
             return true;
         }
-        else {
-            return false;
-        }
-
+        
+        return false;
     }
 
 }
