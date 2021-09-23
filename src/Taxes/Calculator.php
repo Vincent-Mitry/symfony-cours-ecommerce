b80857cac4d9dd539793a96cf1f0ff6af6f55b33
Vincent Mitry <vinz.mitry@gmail.com>
2021-09-23 15:42:56 +0200

    voir le container de services a l'oeuvre

--- src/Taxes/Calculator.php
+++ src/Taxes/Calculator.php
@@ -2,9 +2,19 @@
 
 namespace App\Taxes;
 
+use Psr\Log\LoggerInterface;
+
 class Calculator {
 
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function calcul(float $prix) : float {
+        $this->logger->info("Un calcul a lieu : $prix");
         // On veut calculer le montant de la TVA (tva = 20%) --> 100 => 20
         return $prix * 20 / 100;
     }
