25590df3d44715bfacbb664370d55fec78026204
Vincent Mitry <vinz.mitry@gmail.com>
2021-09-28 12:16:31 +0200

    Découverte d'un repository doctrine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -10,7 +11,17 @@ class HomeController extends AbstractController {
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage() {
+    public function homepage(ProductRepository $productRepository) 
+    {
+        // count([])
+        // find(id)
+        // findBy([], [])
+        // findOneBy([], [])
+        // findAll()
+        $count = $productRepository->count([]);
+
+        dump($count);
+
         return $this->render('home.html.twig');
     }
 }
