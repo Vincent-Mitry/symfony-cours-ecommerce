4a6af9a5770512fd6e87d4a836c7e41db8059c71
Vincent Mitry <vinz.mitry@gmail.com>
2021-09-29 17:43:50 +0200

    creation de la page du formulaire

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -60,4 +60,11 @@ class ProductController extends AbstractController
             'product' => $product,
         ]);
     }
+
+    /**
+     *@Route("/admin/product/create", name="product_create")
+     */
+    public function create() {
+        return $this->render('product/create.html.twig');
+    }
 }
