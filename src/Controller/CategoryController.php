77d3e24b40f1fa4201780e0a10f2912d28408767
Vincent Mitry <vinz.mitry@gmail.com>
2021-10-05 13:12:43 +0200

    exo formulaire de creation de categorie

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,19 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CategoryController extends AbstractController
 {
      /**
      * @Route("/admin/category/create", name="category_create")
      */
-    public function create()
+    public function create(Request $request, EntityManagerInterface $em, SluggerInterface $slugger)
     {
-        return $this->render("category/create.html.twig");
+        $category = new Category;
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $category->setSlug(strtolower($slugger->slug($category->getName())));
+
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute("homepage");
+        }
+
+        $formView = $form->createView();
+
+        return $this->render("category/create.html.twig", [
+            'formView' => $formView 
+        ]);
     }
 
       /**
