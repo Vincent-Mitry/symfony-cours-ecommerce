61e6c319c3698dd596ce421d17be72e62e6e81a1
Vincent Mitry <vinz.mitry@gmail.com>
2021-10-21 17:59:44 +0200

    finalisations: incrémenter, décrémenter, supprimer les éléments du panier

--- src/Cart/CartService.php
+++ src/Cart/CartService.php
@@ -38,6 +38,37 @@ class CartService
         $this->session->set('cart', $cart);
     }
 
+    public function remove(int $id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        unset($cart[$id]);
+
+        $this->session->set('cart', $cart);
+    }
+
+    public function decrement(int $id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        if(!array_key_exists($id, $cart))
+        {
+            return;
+        }
+
+        // Soit le produit est à 1 alors il faut simplement le supprimer
+        if($cart[$id] === 1)
+        {
+            $this->remove($id);
+            return;
+        }
+
+        // Soit le produit est à plus de 1, alors il faut décrémenter
+        $cart[$id] --;
+
+        $this->session->set('cart', $cart);
+    }
+
     public function getTotal(): int 
     {
         $total = 0;
