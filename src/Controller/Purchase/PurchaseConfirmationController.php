287c693ceab08dd25c9e0c554ff3d1ac839d5ef6
Vincent Mitry <vinz.mitry@gmail.com>
2021-10-26 15:30:38 +0200

    refactoring du controller

--- src/Controller/Purchase/PurchaseConfirmationController.php
+++ src/Controller/Purchase/PurchaseConfirmationController.php
@@ -2,71 +2,63 @@
 
 namespace App\Controller\Purchase;
 
-use App\Cart\CartService;
+use DateTime;
+use Twig\Environment;
 use App\Entity\Purchase;
+use App\Cart\CartService;
 use App\Entity\PurchaseItem;
 use App\Form\CartConfirmationType;
-use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\RouterInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Security\Core\Security;
-use Twig\Environment;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 
-class PurchaseConfirmationController
+class PurchaseConfirmationController extends AbstractController
 {
-    protected $formFactory;
-    protected $router;
-    protected $security;
     protected $cartService;
     protected $em;
     
-    public function __construct(FormFactoryInterface $formFactory, RouterInterface $router, Security $security, CartService $cartService, EntityManagerInterface $em){
-        $this->formFactory = $formFactory;
-        $this->router = $router;
-        $this->security = $security;
+    public function __construct(CartService $cartService, EntityManagerInterface $em){
         $this->cartService = $cartService;
         $this->em = $em;
     }
 
     /**
      * @Route("/purchase/confirm", name="purchase_confirm")
+     * @IsGranted("ROLE_USER", message="Vous devez être connecté pour confirmer une commande")
      */
-    public function confirm(Request $request, FlashBagInterface $flashbag)
+    public function confirm(Request $request)
     {
         // 1. Nous voulons lire les données du formulaire
         // FormFactoryInterface / Request
 
-        $form = $this->formFactory->create(CartConfirmationType::class);
+        $form = $this->createForm(CartConfirmationType::class);
 
         $form->handleRequest($request);
 
         // 2. Si le formulaire n'as pas été soumis : dégager
         if(!$form->isSubmitted()) {
             // Message flash puis redirection (FlashbagInterface)
-            $flashbag->add('warning', 'Vous devez remplir le formulaire de confirmation');
-            return new RedirectResponse($this->router->generate('cart_show'));
+            $this->addFlash('warning', 'Vous devez remplir le formulaire de confirmation');
+            return $this->redirectToRoute('cart_show');
         }
 
         // 3. Si je ne suis pas connecté : dégager (Security)
-        $user = $this->security->getUser();
-
-        if(!$user) {
-            throw new AccessDeniedException("Vous devez être connecté pour confirmer une commande");
-        }
+        $user = $this->getUser();
 
         // 4. Si il n'y a pas de produits dans mon panier : dégager (CartService)
         $cartItems = $this->cartService->getDetailedCartItems();
 
         if(count($cartItems) === 0){
-            $flashbag->add('warning', 'Vous ne pouvez pas confirmer une commande avec un panier vide');
-            return new RedirectResponse($this->router->generate('cart_show'));
+            $this->addFlash('warning', 'Vous ne pouvez pas confirmer une commande avec un panier vide');
+            return $this->redirectToRoute('cart_show');
         }
         
         // 5. Nous allons créer une Purchase
@@ -98,9 +90,8 @@ class PurchaseConfirmationController
         // 8. Nous allons enregistrer la commande (EntityManagerInterface)
         $this->em->flush();
 
-        $flashbag->add('success', 'La commande a bien été enregistrée');
+        $this->addFlash('success', 'La commande a bien été enregistrée');
 
-        return new RedirectResponse($this->router->generate('purchase_index'));
-
+        return $this->redirectToRoute('purchase_index');
     }
 }
